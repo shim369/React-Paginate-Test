4140a8e9c5678fad0a0ec0098484742c973e9aae
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-07-15 16:24:20 +0900

    first commit

---
+++ src/Pagination.tsx
@@ -0,0 +1,54 @@
+import React,{ useState } from 'react'
+import "./Pagination.css"
+import AlbumList from './AlbumList';
+import Album from "./type";
+import ReactPaginate from 'react-paginate';
+
+
+type Props = {
+    albums: Album[];
+}
+
+const Pagination = (props: Props) => {
+    const { albums } = props;
+
+    const itemsPerPage = 6;
+
+    const [itemsOffset,setItemsOffset] = useState(0);
+
+    const endOffset = itemsOffset + itemsPerPage;
+    const currentAlbums = albums.slice(itemsOffset, endOffset);
+    const pageCount = Math.ceil(albums.length / itemsPerPage);
+
+    const handlePageClick = (e: {selected: number}) => {
+        const newOffset = (e.selected * itemsPerPage) % albums.length;
+        setItemsOffset(newOffset);
+    };
+
+  return (
+    <div className='albumWrapper'>
+        <AlbumList albums={albums} currentAlbums={currentAlbums} />
+        <ReactPaginate
+        nextLabel="next >"
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        />
+    </div>
+  )
+}
+
+export default Pagination
